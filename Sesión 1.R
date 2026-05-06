e753e609503aef61f838e62e40cdf7c4be2f9842
Cesar Martinez <53794222+Cesar-mtz@users.noreply.github.com>
2026-05-06 13:26:37 -0600

    Add files via upload

---
+++ Sesión 1.R
@@ -0,0 +1,15 @@
+# Estadistica en la Investigación 2026
+# Repositorio del programa de Doctorado Estadística en la Investigación DCMRN 
+
+# César Aldair Martínez Gauna
+
+# Matrícula: 1591819
+
+####################################################################
+# Contenido del curso
+
+**Sesión 1**: Fecha 26/01/26
+
++ Crear cuenta de Github
++ Crear repositorio del curso
++ Primera sincronización
